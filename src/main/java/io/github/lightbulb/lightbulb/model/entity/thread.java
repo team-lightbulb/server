f343819323b3bed7de0f9808ee17dffb18182161
Richmond <rbaafi@cnm.edu>
2020-02-15 16:49:47 -0700

    Created entities.

---
+++ src/main/java/io/github/lightbulb/lightbulb/model/entity/thread.java
@@ -0,0 +1,83 @@
+package io.github.lightbulb.lightbulb.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.Date;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.UUID;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.apache.catalina.User;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.lang.NonNull;
+
+@Entity
+@Table(
+    indexes = {
+        @Index(columnList = "created")
+    }
+)
+public class thread {
+
+  @NonNull
+  @Id
+  @GeneratedValue(generator = "uuid2")
+  @GenericGenerator(name = "uuid2", strategy = "uuid2")
+  @Column(name = "thread_id", columnDefinition = "CHAR(16) FOR BIT DATA",
+      nullable = false, updatable = false)
+  private UUID id;
+
+  @NonNull
+  @CreationTimestamp
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(nullable = false, updatable = false)
+  private Date created;
+
+  @NonNull
+  @UpdateTimestamp
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(nullable = false)
+  private Date updated;
+
+  @NonNull
+  @Column(length = 1024, nullable = false, unique = true)
+  private String name;
+
+  @NonNull
+  public Date getCreated() {
+    return created;
+  }
+
+  public void setCreated(@NonNull Date created) {
+    this.created = created;
+  }
+
+  @NonNull
+  public Date getUpdated() {
+    return updated;
+  }
+
+  public void setUpdated(@NonNull Date updated) {
+    this.updated = updated;
+  }
+
+  @NonNull
+  public String getName() {
+    return name;
+  }
+
+  public void setName(@NonNull String name) {
+    this.name = name;
+  }
+}
---
+++ src/main/java/io/github/lightbulb/lightbulb/model/entity/user.java
@@ -0,0 +1,80 @@
+package io.github.lightbulb.lightbulb.model.entity;
+
+import java.util.Date;
+import java.util.UUID;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.lang.NonNull;
+
+@Entity
+@Table(
+    indexes = {
+        @Index(columnList = "created")
+    }
+)
+
+public class user {
+
+  @NonNull
+  @Id
+  @GeneratedValue(generator = "uuid2")
+  @GenericGenerator(name = "uuid2", strategy = "uuid2")
+  @Column(name = "user_id", columnDefinition = "CHAR(16) FOR BIT DATA",
+      nullable = false, updatable = false)
+  private UUID id;
+
+  @NonNull
+  @CreationTimestamp
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(nullable = false, updatable = false)
+  private Date created;
+
+  @NonNull
+  @UpdateTimestamp
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(nullable = false)
+  private Date updated;
+
+  @NonNull
+  @Column(length = 4096, nullable = false, unique = true)
+  private String text;
+
+  @NonNull
+  public Date getCreated() {
+    return created;
+  }
+
+  public void setCreated(@NonNull Date created) {
+    this.created = created;
+  }
+
+  @NonNull
+  public Date getUpdated() {
+    return updated;
+  }
+
+  public void setUpdated(@NonNull Date updated) {
+    this.updated = updated;
+  }
+
+  @NonNull
+  public String getText() {
+    return text;
+  }
+
+  public void setText(@NonNull String text) {
+    this.text = text;
+  }
+}
