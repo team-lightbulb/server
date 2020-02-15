d69366e89d844c96438abfccd5bdf702008e76a8
Richmond <rbaafi@cnm.edu>
2020-02-15 15:57:41 -0700

    Initial commit.

---
+++ src/test/java/io/github/lightbulb/lightbulb/LightbulbApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lightbulb.lightbulb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightbulbApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
