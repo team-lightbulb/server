ea284e6b3af8a6770cf4c268be4e2556e0cfbd8b
Richmond <rbaafi@cnm.edu>
2020-03-10 10:16:44 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/edu/cnm/deepdive/team-lightbulb/LightbulbApplication.java
    #	src/main/java/edu/cnm/deepdive/team-lightbulb/model/entity/keyword.java
    #	src/main/java/edu/cnm/deepdive/team-lightbulb/model/entity/thread.java
    #	src/main/java/edu/cnm/deepdive/team-lightbulb/model/entity/user.java

--- src/main/java/edu/cnm/deepdive/lightbulb/controller/rest/UserController.java
+++ src/main/java/edu/cnm/deepdive/lightbulb/controller/rest/UserController.java
@@ -1,5 +1,17 @@
 package edu.cnm.deepdive.lightbulb.controller.rest;
 
+import edu.cnm.deepdive.lightbulb.model.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserController {
+
+  private final UserRepository userRepository;
+
+  @Autowired
+  public UserController(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
 
 }
