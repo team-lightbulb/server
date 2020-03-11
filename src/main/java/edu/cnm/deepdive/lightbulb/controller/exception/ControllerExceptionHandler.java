69a1c5e707ce5d12b370faf17f2311b462c210da
Jonathan Padilla <jpadilla8104@gmail.com>
2020-03-11 10:31:15 -0600

    Added javadocs

--- src/main/java/edu/cnm/deepdive/lightbulb/controller/exception/ControllerExceptionHandler.java
+++ src/main/java/edu/cnm/deepdive/lightbulb/controller/exception/ControllerExceptionHandler.java
@@ -6,17 +6,31 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+/***
+ * This method sends a bad request response if a exception is caught.
+ */
 @RestControllerAdvice
 public class ControllerExceptionHandler {
 
+
+  /***
+   * This method sends a bad request response if a exception is caught.
+   */
   @ExceptionHandler(SearchTermTooShortException.class)
   @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Search term too short")
   public void tooShort() {}
 
+
+  /***
+   * Sends exception if resource not found if and exception is caught.
+   */
   @ExceptionHandler(NoSuchElementException.class)
   @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Resource not found")
   public void notFound() {}
 
+  /***
+   * This method sends a bad request response if a exception is caught.
+   */
   @ExceptionHandler(IllegalArgumentException.class)
   @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Invalid request content or parameters")
   public void badRequest() {}
--- src/main/java/edu/cnm/deepdive/lightbulb/controller/exception/SearchTermTooShortException.java
+++ src/main/java/edu/cnm/deepdive/lightbulb/controller/exception/SearchTermTooShortException.java
@@ -1,22 +1,40 @@
 package edu.cnm.deepdive.lightbulb.controller.exception;
 
+/***
+ * Throws illegal argument exception.
+ */
 public class SearchTermTooShortException extends IllegalArgumentException {
 
 
+  /***
+   * Throws illegal argument exception.
+   */
   public SearchTermTooShortException() {
     super();
   }
 
 
+  /***
+   * Throws illegal argument exception.
+   */
   public SearchTermTooShortException(String s) {
     super(s);
   }
 
+
+  /***
+   * Throws illegal argument exception.
+   */
   public SearchTermTooShortException(String message, Throwable cause) {
     super(message, cause);
   }
 
+
+  /***
+   * Throws illegal argument exception.
+   */
   public SearchTermTooShortException(Throwable cause) {
     super(cause);
   }
+
 }
